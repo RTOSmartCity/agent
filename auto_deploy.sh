b246d040226af9ba2fba6fae6854aaf2ab0ae21e
Joshua Panza <joshuaapanza@gmail.com>
2025-04-09 15:22:15 -0400

    auto_deploy changes

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -5,6 +5,7 @@ IP2="34.194.7.63"
 
 USERNAME="qnxuser"
 REMOTE_DIR="/home/qnxuser"
+CFG_DIR="configs"
 
 scp server "$USERNAME@$IP1:$REMOTE_DIR"
 
@@ -12,12 +13,16 @@ scp vehicle "$USERNAME@$IP2:$REMOTE_DIR"
 
 scp trafficlight "$USERNAME@$IP2:$REMOTE_DIR"
 
+scp $CFG_DIR/traffic_config.txt "$USERNAME@$IP2:$REMOTE_DIR"
+
+scp $CFG_DIR/vehicle_config.txt "$USERNAME@$IP2:$REMOTE_DIR"
+
 
 ssh -i ../blls.pem root@"$IP1" "chmod +x $REMOTE_DIR/server"
 # ssh -i ../blls.pem root@"$IP1" "/bin/ln -s /lib/libsocket.so.4 /lib/libsocket.so.3"
 
 
-ssh -i ../blls.pem root@"$IP2" "chmod +x $REMOTE_DIR/vehicle"
+ssh -i ../blls.pem root@"$IP2" "chmod +x $REMOTE_DIR/vehicle" 
 # ssh -i ../blls.pem root@"$IP2" "/bin/ln -s /lib/libsocket.so.4 /lib/libsocket.so.3"
 
 ssh -i ../blls.pem root@"$IP2" "chmod +x $REMOTE_DIR/trafficlight"
