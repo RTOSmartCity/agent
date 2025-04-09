0ed62b3977103e92190f309ffe05632888dfb1a4
Joshua Panza <joshuaapanza@gmail.com>
2025-04-09 15:50:17 -0400

    Fixed script for new instance

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -2,6 +2,7 @@
 
 IP1="3.226.148.224"
 IP2="34.194.7.63"
+IP3="13.219.193.24"
 
 USERNAME="qnxuser"
 REMOTE_DIR="/home/qnxuser"
@@ -9,21 +10,15 @@ CFG_DIR="configs"
 
 scp server "$USERNAME@$IP1:$REMOTE_DIR"
 
-scp vehicle "$USERNAME@$IP2:$REMOTE_DIR"
+scp vehicle $CFG_DIR/vehicle-config.txt "$USERNAME@$IP2:$REMOTE_DIR"
 
-scp trafficlight "$USERNAME@$IP2:$REMOTE_DIR"
-
-scp $CFG_DIR/traffic_config.txt "$USERNAME@$IP2:$REMOTE_DIR"
-
-scp $CFG_DIR/vehicle_config.txt "$USERNAME@$IP2:$REMOTE_DIR"
-
+scp trafficlight $CFG_DIR/traffic-config.txt "$USERNAME@$IP3:$REMOTE_DIR"
 
 ssh -i ../blls.pem root@"$IP1" "chmod +x $REMOTE_DIR/server"
 # ssh -i ../blls.pem root@"$IP1" "/bin/ln -s /lib/libsocket.so.4 /lib/libsocket.so.3"
 
-
 ssh -i ../blls.pem root@"$IP2" "chmod +x $REMOTE_DIR/vehicle" 
 # ssh -i ../blls.pem root@"$IP2" "/bin/ln -s /lib/libsocket.so.4 /lib/libsocket.so.3"
 
-ssh -i ../blls.pem root@"$IP2" "chmod +x $REMOTE_DIR/trafficlight"
-# ssh -i ../blls.pem root@"$IP2" "/bin/ln -s /lib/libsocket.so.4 /lib/libsocket.so.3"
+ssh -i ../blls.pem root@"$IP3" "chmod +x $REMOTE_DIR/trafficlight"
+# ssh -i ../blls.pem root@"$IP3" "/bin/ln -s /lib/libsocket.so.4 /lib/libsocket.so.3"
